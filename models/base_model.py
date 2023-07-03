99388731ad346404e9bcd3577dd748c437be90d0
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-03 00:46:39 -0700

    refactor kwargs again

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-import models import storage
+import models
 
 
 Base = declarative_base()
@@ -18,22 +18,24 @@ class BaseModel:
 
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """ Instantiates a new model """
-        if kwargs:
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+        """Instantiates a new model"""
+        if id not in kwargs:
+            self.id = str(uuid.uuid4())
+        if "created_at" not in kwargs:
+            self.created_at = datetime.now()
+        else:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-                if 'updated_at' in kwargs:
+
+        if "updated_at" not in kwargs:
+            self.updated_at = datetime.now()
+        else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            if 'id' not in kwargs:
-                kwargs['id'] = str(uuid.uuid4())
-            self.__dict__.update(kwargs)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+        if "__class__" in kwargs:
+            del kwargs['__class__']
 
+        self.__dict__.update(kwargs)
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
@@ -46,7 +48,6 @@ class BaseModel:
         models.storage.save()
 
     def delete(self):
-        """ Deletes the current instance from storage """
         models.storage.delete(self)
 
     def to_dict(self):
