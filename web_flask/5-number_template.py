28230030f7073d7845090458e12fd77647c17afa
PaintRock <6126@holbertonstudents.com>
2023-07-13 16:05:52 -0500

    n or number

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,7 +34,7 @@ def number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """This is going to render an HTML page, if n is a number"""
     return render_template('5-number.html', n=n)
