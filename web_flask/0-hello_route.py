b2d0726b127007d558edd80af419e0e194bc503d
PaintRock <6126@holbertonstudents.com>
2023-07-13 12:09:57 -0500

    doc u mint

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-from flask import Flask
 """This is a file to start the web application"""
+from flask import Flask
 
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-"""This says hello"""
+    """This says hello"""
     return 'Hello HBNB!'
+
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5000)
