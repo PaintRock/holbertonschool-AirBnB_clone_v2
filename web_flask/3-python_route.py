e401b4af701dccb4d4aa0925fd8e0fbc96e6dc26
PaintRock <6126@holbertonstudents.com>
2023-07-13 13:41:44 -0500

    3v2

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This is a file to start the web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """This says hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """This says HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = (text).replace("_", " ")
+    return 'C {}'.format(text)
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    text = (text).replace("_", " ")
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
