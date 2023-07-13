f07937d9542e0c204009778dfbeeac2a3e2b5fbc
PaintRock <6126@holbertonstudents.com>
2023-07-13 15:44:03 -0500

    render

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -36,6 +36,7 @@ def number(n):
 
 @app.route('/number_template'<int:n>, strict_slashes=False)
 def number_template(n):
+    """This is going to render an HTML page, if n is a number"""
     return render_template('5-number.html', number=n)
 
 if __name__ == '__main__':
