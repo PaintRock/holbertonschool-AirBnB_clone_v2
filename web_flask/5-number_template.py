d3d782ac70cdbbaaea5e66ce38befc786541f80e
PaintRock <6126@holbertonstudents.com>
2023-07-13 15:27:21 -0500

    5v2

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This is a file to start the web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """This says hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """This says HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = (text).replace("_", " ")
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    text = (text).replace("_", " ")
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template'<int:n>, strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', number=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/templates/README.md
@@ -0,0 +1,2 @@
+This is the readme for the templates subdirectory of the web_flask 
+directory of the Holbertonschool AirBNB CLONE V2 project. 
