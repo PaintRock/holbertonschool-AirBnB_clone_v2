c7efd923c6307786ecf2c666552790cff3635a5d
PaintRock <6126@holbertonstudents.com>
2023-07-13 12:25:54 -0500

    1v2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,10 +9,6 @@ def hello():
     """This says hello"""
     return 'Hello HBNB!'
 
-def hbnb():
-    """This says HBNB"""
-    return 'HBNB'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This is a file to start the web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """This says hello"""
+    return 'Hello HBNB!'
+
+def hbnb():
+    """This says HBNB"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
