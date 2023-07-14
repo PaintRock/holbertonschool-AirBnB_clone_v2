679377d72db0bd95861647be9ec0381b85ac9c3f
PaintRock <6126@holbertonstudents.com>
2023-07-14 15:29:31 -0500

    odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,10 +42,10 @@ def number_template(n):
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even (n):
+def number_odd_or_even(n):
     """Displays the HTML page only if n is an integer"""
     return render_template('6-number_odd_or_even.html', n=n)
-    
-    
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
