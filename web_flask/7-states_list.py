0b83d77a61317bf20334bccc32245da0966ecb23
PaintRock <6126@holbertonstudents.com>
2023-07-15 05:34:07 -0500

    blankspace

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,16 +4,19 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-#Create the Flask application
+# Create the Flask application
 app = Flask(__name__)
 
-#Define the teardown_appcontext method to close the SQLAlchemy session after each request
+
+""" Define the teardown_appcontext method to close the
+SQLAlchemy session after each request """
 @app.teardown_appcontext
 def teardown_db(exception):
     """This defines the teardown"""
     storage.close()
 
-#Define the /state_list route
+
+# Define the /state_list route
 @app.route('/state_list', strict_slashes=False)
 def states_list():
     """Fetch all State objects from the DBStorage sorted by name (A->Z)"""
