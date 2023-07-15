5103e5a44f28dc2a753f2ab55f86926c42329685
PaintRock <6126@holbertonstudents.com>
2023-07-15 15:17:51 -0500

    forgot to nothi

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -18,19 +18,18 @@ def teardown_session(exception=None):
 def states_list():
     """Render the HTML page displaying the list of states."""
     states = sorted(storage.all(State).values(), key=lambda x: x.name)
-    return render_template(\
-        'states_and_cities.html', states=states, cities=None)
+    return render_template('states_and_cities.html', states=states, cities=None)
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def cities_list(id):
     """Render the HTML page displaying cities for a specific state."""
-    state = storage.get(State, id)
-    if state is None:
+    st = storage.get(State, id)
+    if st is None:
         return render_template('states_and_cities.html', states=None,
         cities=None)
 
-    cities = sorted(state.cities, key=lambda x: x.name)
+    cities = sorted(st.cities, key=lambda x: x.name)
     return render_template('states_and_cities.html', states=None,
         cities=cities)
 
